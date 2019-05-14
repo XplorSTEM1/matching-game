6592153de4c18c64d58ae2a5d38edaa1bad5bad7
XplorSTEM1 <44484404+XplorSTEM1@users.noreply.github.com>
2019-05-14 00:36:42 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
 const cards = document.querySelectorAll('.memory-card');
+// declaring move variable
+let moves = 0;
+let counter = document.querySelector(".moves");
 
 let hasFlippedCard = false;
 let lockBoard = false;
@@ -51,6 +54,51 @@ function unflipCards() {
 function resetBoard() {
   [hasFlippedCard, lockBoard] = [false, false];
   [firstCard, secondCard] = [null, null];
+ // reset moves
+    moves = 0;
+    counter.innerHTML = moves;
+    //reset timer
+    second = 0;
+    minute = 0; 
+    hour = 0;
+    var timer = document.querySelector(".timer");
+    timer.innerHTML = "0 mins 0 secs";
+    clearInterval(interval);
+}
+// @description count player's moves
+function moveCounter(){
+    moves++;
+    counter.innerHTML = moves;
+    //start timer on first click
+    if(moves == 1){
+        second = 0;
+        minute = 0; 
+        hour = 0;
+        startTimer();
+    }
+}
+// @description game timer
+var second = 0, minute = 0; hour = 0;
+var timer = document.querySelector(".timer");
+var interval;
+function startTimer(){
+    interval = setInterval(function(){
+        timer.innerHTML = minute+"mins "+second+"secs";
+        second++;
+        if(second == 60){
+            minute++;
+            second=0;
+        }
+        if(minute == 60){
+            hour++;
+            minute = 0;
+        }
+    },1000);
+}
+// @desciption for user to play Again 
+function playAgain(){
+    modal.classList.remove("show");
+    resetBoard();
 }
 
 (function shuffle() {
